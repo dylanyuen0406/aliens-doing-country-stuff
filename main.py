d2d566d9427a910226bf84c577b18ae31341b8b1
Dylan Yuen <dylanyuentym@gmail.com>
2023-10-22 13:04:52 +0800

    country 1

---
+++ main.py
@@ -0,0 +1,40 @@
+age=int(input("What is your Age"))
+if age <= 50:
+  personality = input("Are you human")
+  if personality=="no":
+    print("AAAAAHHHHH")
+  elif personality=="yes":
+    IQ=input("Which continent is Kazakhstan in?")
+    if IQ=="Asia":
+      difficult=int(input("How many colours are on the flag of Gabon?"))
+      if difficult == 3:
+        hardcore=int(input("You've made it this far.How many colours are on the flag of Comoros?"))
+        if hardcore==5:
+          print("IQ 10000")
+        else:
+          print("wow amazing you made this far but can improve")
+      else:
+        print("...okay")
+    else:
+      print("sigh")
+  else:
+    print("wut")
+else:
+  confirm=input("ARE YOU REALLY HUMAN")
+  if confirm == "no":
+    print("ARGGGGGGGHHHHHHH")
+  elif confirm=="yes":
+    prank= input("Since you're so old,which continent does San Marino belong to?")
+    if prank =="Europe":
+      absolute_crusher =input("ok now big brain which country borders Slovakia,and formed a country together in the past?")
+      if absolute_crusher =="Czechia":
+        print("wow...just wow")
+      else:
+        print("not bad not bad though answer was wrong but GOOD TRY")
+    else:
+      print("good try but... yeah")
+  else:
+    print("I have no time for you to prank me")
+    
+      
+
